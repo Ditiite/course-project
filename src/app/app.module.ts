5a9065bec8c44aca1705a0313460b86f933b7c84
Dita <dita.rahmane.silova@inbox.lv>
2020-09-24 21:38:26 +0300

    Basic navigation. Couple solutions.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,11 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // RouterModule.forRoot([
+    //   { path: 'app-recipe-list', component: RecipeListComponent},
+    //   { path: 'app-shopping-list', component: ShoppingListComponent}
+    // ])
   ],
   providers: [],
   bootstrap: [AppComponent]
